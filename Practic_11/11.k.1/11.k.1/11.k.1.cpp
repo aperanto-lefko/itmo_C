a8ccc5e74e9e84f57d9886c00436b9fad5e7c34f
aperanto-lefko <polarster@yandex.ru>
2025-10-17 22:26:57 +0300

    Работа 11

---
+++ Practic_11/11.k.1/11.k.1/11.k.1.cpp
@@ -0,0 +1,89 @@
+﻿
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+#include <windows.h>
+class Time {
+private:
+    int hours;
+    int minutes;
+    int seconds;
+
+    void normalize() {
+        int totalSeconds = hours * 3600 + minutes * 60 + seconds;
+        if (totalSeconds < 0) totalSeconds = 0;
+        seconds = totalSeconds % 60;
+        totalSeconds /= 60;
+        minutes = totalSeconds % 60;
+        hours = totalSeconds / 60;
+    }
+
+public:
+    Time() : hours(0), minutes(0), seconds(0) {}
+
+    Time(int h, int m, int s) : hours(h), minutes(m), seconds(s) {
+        if (h < 0 || m < 0 || s < 0) {
+            throw invalid_argument("Время не может быть отрицательным");
+        }
+       
+        normalize();
+    }
+
+    void display() const {
+        cout << (hours < 10 ? "0" : "") << hours << ":"
+            << (minutes < 10 ? "0" : "") << minutes << ":"
+            << (seconds < 10 ? "0" : "") << seconds;
+    }
+
+    Time operator+(const Time& other) const {
+        return Time(hours + other.hours, minutes + other.minutes, seconds + other.seconds);
+    }
+
+    Time operator-(const Time& other) const {
+        return Time(hours - other.hours, minutes - other.minutes, seconds - other.seconds);
+    }
+
+    Time operator+(double sec) const {
+        return Time(hours, minutes, seconds + static_cast<int>(sec));
+    }
+
+    friend Time operator+(double sec, const Time& t) {
+        return t + sec;
+    }
+
+    bool operator==(const Time& other) const {
+        return hours == other.hours && minutes == other.minutes && seconds == other.seconds;
+    }
+};
+
+int main() {
+    SetConsoleOutputCP(1251);
+    try {
+        Time t1(2, 30, 45);
+        Time t2(1, 15, 20);
+
+        cout << "t1: "; t1.display(); cout << endl;
+        cout << "t2: "; t2.display(); cout << endl;
+
+        Time sum = t1 + t2;
+        cout << "t1 + t2: "; sum.display(); cout << endl;
+
+        Time diff = t1 - t2;
+        cout << "t1 - t2: "; diff.display(); cout << endl;
+
+        Time timePlusDouble = t1 + 30.5;
+        cout << "t1 + 30.5: "; timePlusDouble.display(); cout << endl;
+
+        Time doublePlusTime = 45.2 + t2;
+        cout << "45.2 + t2: "; doublePlusTime.display(); cout << endl;
+
+        cout << "t1 == t2: " << (t1 == t2 ? "true" : "false") << endl;
+
+       
+    }
+    catch (const exception& e) {
+        cout << "Общая ошибка: " << e.what() << endl;
+    }
+
+    return 0;
+}
---
+++ Practic_11/11.k.2/11.k.2/11.k.2.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <windows.h>
+
+class Point {
+private:
+    double x;
+    double y;
+
+public:
+    Point(double x = 0, double y = 0) : x(x), y(y) {}
+
+    // расчет расстояния до центра координат
+    double distanceToOrigin() const {
+        return std::sqrt(x * x + y * y);
+    }
+
+    // Перегрузка  < для сравнения по расстоянию до центра
+    bool operator<(const Point& other) const {
+        return distanceToOrigin() < other.distanceToOrigin();
+    }
+
+    // Перегрузка <<
+    friend std::ostream& operator<<(std::ostream& os, const Point& point) {
+        os << "(" << point.x << ", " << point.y << ")";
+        return os;
+    }
+};
+
+int main()
+
+{
+    SetConsoleOutputCP(1251);
+    std::vector<Point> v;
+    v.push_back(Point(1, 2));
+    v.push_back(Point(10, 12));
+    v.push_back(Point(21, 7));
+    v.push_back(Point(4, 8));
+
+    std::sort(v.begin(), v.end()); 
+
+    for (auto& point : v)
+        std::cout << point << " -> distance: " << point.distanceToOrigin() << '\n';
+
+    return 0;
+}
